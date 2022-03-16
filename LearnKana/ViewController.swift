2a8b5e2da5927ce1201c003253392d46d635e08c
Jesus Escribano <jesus.escribano@nuvolar.eu>
2022-03-16 14:40:58 +0100

    Initial Commit

---
+++ LearnKana/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LearnKana
+//
+//  Created by Jesus Escribano on 16/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LearnKanaUITests/LearnKanaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  LearnKanaUITestsLaunchTests.swift
+//  LearnKanaUITests
+//
+//  Created by Jesus Escribano on 16/3/22.
+//
+
+import XCTest
+
+class LearnKanaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
